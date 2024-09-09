87d4e3260b9d4a9a4c791cf2253d322d0daa288b
Anirudh Kulkarni <askakdagr8@gmail.com>
2024-09-09 15:44:29 -0500

    refactor: Remove duplicate code and add placeholders for drag and drop in TaskList

--- src/TaskList.js
+++ src/TaskList.js
@@ -2,24 +2,6 @@ import React from 'react';
 import TaskCard from './TaskCard';
 
 function TaskList({ title, tasks, onUpdateStatus }) {
-  return (
-    <div className="task-list">
-      <h2>{title}</h2>
-      {tasks.map(task => (
-        <TaskCard 
-          key={task.id} 
-          task={task} 
-          onUpdateStatus={onUpdateStatus} 
-        />
-      ))}
-    </div>
-  );
-}
-
-export default TaskList;
-import TaskCard from './TaskCard';
-
-function TaskList({ title, tasks, onUpdateStatus }) {
   // TODO: Implement drag and drop functionality
   const handleDragOver = (e) => {
     e.preventDefault();
